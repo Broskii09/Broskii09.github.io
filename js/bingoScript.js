5465e73a8139db0d0ae2e5c746562f82efe25aae
Broskii09 <broskii.09@gmail.com>
2024-04-24 15:18:56 -0500

    Styling update

--- js/bingoScript.js
+++ js/bingoScript.js
@@ -1,6 +1,7 @@
 // Accessing HTML elements that will hold the bingo board and display call history.
 const bingoBoard = document.getElementById('bingo-board');
 const callHistoryDisplay = document.getElementById('call-history');
+const titleElement = document.querySelector('h1'); // Accessing the H1 element for title updates
 let callHistory = [];
 
 // Creates a single bingo cell. This function is versatile for both header and number cells.
@@ -19,8 +20,6 @@ function createBingoCell(letter, number, isHeader = false) {
     }
     return cell;
 }
-
-
 
 // Updates the display of the last three numbers called.
 function updateCallHistory(call) {
@@ -51,81 +50,55 @@ function generateBingoBoard() {
   });
 }
 
-// Applies a visual pattern to the bingo board for easy recognition of the winning pattern.
-function applyBingoPattern() {
-  const rows = document.querySelectorAll('.bingo-row');
-  rows.forEach((row, rowIndex) => {
-    const cells = row.querySelectorAll('.bingo-cell:not(.fw-bold)');
-    cells.forEach((cell, cellIndex) => {
-      // Apply a diagonal pattern by adding a special class to these cells.
-      if (rowIndex === cellIndex) { // This creates a diagonal from top left to bottom right.
-        cell.classList.add('pattern-cell');
-      }
-    });
-  });
+// Event listener for the theme toggle button
+function toggleTheme() {
+    document.body.classList.toggle('dark-theme');
+    // Update the title based on the theme
+    titleElement.textContent = document.body.classList.contains('dark-theme') ? 'Bingo Tracker' : 'Bingo Tracker';
+    // Save the current theme to localStorage
+    localStorage.setItem('theme', document.body.classList.contains('dark-theme') ? 'dark' : 'light');
 }
 
-// Define patterns as objects with name and grid properties
-const patterns = {
-  xPattern: [
-    [true, false, false, false, true],
-    [false, true, false, true, false],
-    [false, false, true, false, false],
-    [false, true, false, true, false],
-    [true, false, false, false, true]
-  ],
-  blockParty: [
-    [true, true, false, true, true],
-    [true, true, false, true, true],
-    [false, false, false, false, false],
-    [true, true, false, true, true],
-    [true, true, false, true, true]
-  ],
-  centSign: [
-    [false, true, false, true, false],
-    [true, false, true, false, true],
-    [false, true, false, true, false],
-    [true, false, true, false, true],
-    [false, true, false, true, false]
-  ],
-  cloverLeaf: [
-    [true, false, false, false, true],
-    [false, false, false, false, false],
-    [true, false, true, false, true],
-    [false, false, false, false, false],
-    [true, false, false, false, true]
-  ],
-  coverAll: [
-    [true, true, true, true, true],
-    [true, true, true, true, true],
-    [true, true, true, true, true],
-    [true, true, true, true, true],
-    [true, true, true, true, true]
-  ],
-  crazyKite: [
-    [false, false, true, false, false],
-    [false, true, true, false, false],
-    [true, true, true, true, true],
-    [false, true, true, false, false],
-    [false, false, true, false, false]
-  ],
-  crazyPyramid: [
-    [true, true, true, true, true],
-    [true, true, true, true, false],
-    [true, true, true, false, false],
-    [true, true, false, false, false],
-    [true, false, false, false, false]
-  ],
-  TforTiki: [
-    [true, true, true, true, true],
-    [false, false, true, false, false],
-    [false, false, true, false, false],
-    [false, false, true, false, false],
-    [false, false, true, false, false]
-  ],
-};
+// Check localStorage for a saved theme and apply it on page load
+function applySavedTheme() {
+    if (localStorage.getItem('theme') === 'dark') {
+        document.body.classList.add('dark-theme');
+        titleElement.textContent = 'Bingo Tracker'; // Set the correct title for dark mode
+    } else {
+        titleElement.textContent = 'Bingo Tracker'; // Set the correct title for light mode
+    }
+}
 
+document.addEventListener('DOMContentLoaded', function() {
+    // Generate the bingo board and the initial pattern board
+    generateBingoBoard();
 
+    // Initialize theme toggle button
+    const themeToggleButton = document.getElementById('theme-toggle');
+    if (themeToggleButton) {
+        themeToggleButton.addEventListener('click', toggleTheme);
+    }
+
+    // Apply saved theme
+    applySavedTheme();
+
+    // Attach event listener to pattern selector dropdown
+    document.getElementById('pattern-select').addEventListener('change', function() {
+        const selectedPattern = this.value;
+        generatePatternBoard(patterns[selectedPattern]);
+    });
+});
+
+// Adjust heights when resizing the window
+window.addEventListener('resize', adjustElementHeights);
+
+// Adjust heights on load and when resizing the window
+function adjustElementHeights() {
+    let headerHeight = document.querySelector('h1').offsetHeight;
+    let controlsHeight = document.querySelector('.row').offsetHeight;
+    let availableHeight = window.innerHeight - headerHeight - controlsHeight;
+    document.getElementById('bingo-board').style.height = `${availableHeight}px`;
+}
 
 // Generate the pattern board based on the selected pattern
 function generatePatternBoard(patternGrid) {
@@ -147,53 +120,3 @@ function generatePatternBoard(patternGrid) {
         patternBoard.appendChild(patternRow);
     });
 }
-
-// Adjust heights on load and when resizing the window
-function adjustElementHeights() {
-    let headerHeight = document.querySelector('h1').offsetHeight;
-    let controlsHeight = document.querySelector('.row').offsetHeight;
-    let availableHeight = window.innerHeight - headerHeight - controlsHeight;
-    document.getElementById('bingo-board').style.height = `${availableHeight}px`;
-}
-
-// Event listener for the theme toggle button
-function toggleTheme() {
-    document.body.classList.toggle('dark-theme');
-    // Save the current theme to localStorage
-    localStorage.setItem('theme', document.body.classList.contains('dark-theme') ? 'dark' : 'light');
-}
-
-// Check localStorage for a saved theme and apply it on page load
-function applySavedTheme() {
-    if (localStorage.getItem('theme') === 'dark') {
-        document.body.classList.add('dark-theme');
-    }
-}
-
-document.addEventListener('DOMContentLoaded', function() {
-    // Generate the bingo board and the initial pattern board
-    generateBingoBoard();
-    const initialPattern = document.getElementById('pattern-select').value;
-    generatePatternBoard(patterns[initialPattern]);
-
-    // Attach change event listener to pattern selector dropdown
-    document.getElementById('pattern-select').addEventListener('change', function() {
-        const selectedPattern = this.value;
-        generatePatternBoard(patterns[selectedPattern]);
-    });
-
-    // Initialize theme toggle button
-    const themeToggleButton = document.getElementById('theme-toggle');
-    if (themeToggleButton) {
-        themeToggleButton.addEventListener('click', toggleTheme);
-    }
-
-    // Apply saved theme
-    applySavedTheme();
-
-    // Adjust element heights
-    adjustElementHeights();
-});
-
-// Adjust heights when resizing the window
-window.addEventListener('resize', adjustElementHeights);
